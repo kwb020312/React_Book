efa01b6264b2d141ab13c46e9bec4b056e558209
kwb020312 <kwb020312@naver.com>
2019-10-21 12:07:46 +0900

    map 반복문 filter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from 'react';
-import './App.css';
-import Header from './MyComponent';
-import Counter from './Counter';
-import Say from './Say';
-import Event from './EventPractice';
-import Validated from './ValidationSample';
-import Scroll from './ScrollBox';
+import Map from './map'
 const App = () => {
   return (
     <>
-      <p><Header name="김우빈" children="Hello React">Hello React</Header></p>
-      <p><Counter /></p>
-      <p><Say/></p>
-      <p><Event/></p>
-      <p><Validated/></p>
-      <p><Scroll/></p>
+      <p><Map/></p>
     </>
   )
 }
