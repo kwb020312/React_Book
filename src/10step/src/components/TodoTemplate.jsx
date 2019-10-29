82be8a6d26a3144a4f2aab85380d25628c9b3a82
kwb020312 <kwb020312@naver.com>
2019-10-29 16:20:10 +0900

    TodoList

--- src/10step/src/components/TodoTemplate.jsx
+++ src/10step/src/components/TodoTemplate.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import './TodoTemplate.scss';
+import React from 'react';
+import './TodoTemplate.css';
 
-const TodoTemplate = (children) => {
-    return (
-        <div className="TodoTemplate">
-            <div className="app-title">일정관리</div>
-            <div className="content">{children}</div>
-        </div>
-    )
-}
+const TodoTemplate = ({ children }) => {
+  return (
+    <div className="TodoTemplate">
+      <div className="app-title">일정 관리</div>
+      <div className="content">{children}</div>
+    </div>
+  );
+};
 
 export default TodoTemplate;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
-import React from 'react';
+import React, { useState, useRef, useCallback } from 'react';
+import TodoTemplate from './10step/src/components/TodoTemplate';
+import TodoInsert from './10step/src/components/TodoInsert';
+import TodoList from './10step/src/components/TodoList.js';
 import './10step/index.css'
-import TodoTemplate from './10step/src/components/TodoTemplate'
+
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
+
+  // 고유 값으로 사용 될 id
+  // ref 를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; // nextId 1 씩 더하기
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos.map(todo =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
   return (
-    <>
-      <TodoTemplate>Todo App을 만들자!</TodoTemplate>
-    </>
-  )
-}
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+};
 
 export default App;
